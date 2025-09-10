0a1344d72193fe25e964069e7583d002a49dcdc0
DARK OSINT (Tiga Serangkai) <levianawulandari7@gmail.com>
2025-09-10 20:29:36 +0700

    Create logger.py

---
+++ server/logger.py
@@ -0,0 +1,21 @@
+import logging
+
+def setup_logger(log_file):
+    logger = logging.getLogger('server_logger')
+    logger.setLevel(logging.DEBUG)
+
+    fh = logging.FileHandler(log_file)
+    fh.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+
+    if not logger.hasHandlers():
+        logger.addHandler(fh)
+        logger.addHandler(ch)
+
+    return logger
